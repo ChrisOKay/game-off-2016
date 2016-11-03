6c2c5049bdbf13cbb58e61cb31c0a84149e40cc9
Thomas Karcher <t_karcher@posteo.de>
2016-11-03 22:58:17 +0100

    Rain and background scrolling implemented.

---
+++ game/BackgroundScrolling.gd
@@ -0,0 +1,18 @@
+
+extends ParallaxLayer
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var mypos = get_pos()
+	mypos.x -= 60 * delta
+	if mypos.x < -1267: mypos.x = 0
+	set_pos(mypos)
+
