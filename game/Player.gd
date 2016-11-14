44685a6a9cf8d4c9587bcdcd4d07487a035bd2e9
MrPottwal <aaron.n@web.de>
2016-11-14 22:27:32 +0100

    I Testgrafiken

--- game/Player.gd
+++ game/Player.gd
@@ -12,16 +12,16 @@ func _ready():
 	
 func _fixed_process(delta):
 	var mypos = get_pos()
-	if Input.is_key_pressed(KEY_LEFT):
+	if Input.is_key_pressed(KEY_A):
 		mypos.x -= 350 * delta
 		get_node("AnimatedSprite").set_flip_h(true)
-	if Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_key_pressed(KEY_D):
 		mypos.x += 350 * delta
 		get_node("AnimatedSprite").set_flip_h(false)
-	if Input.is_key_pressed(KEY_UP):
+	if Input.is_key_pressed(KEY_W):
 		mypos.y -= 350 * delta
 		set_z(mypos.y)
-	if Input.is_key_pressed(KEY_DOWN):
+	if Input.is_key_pressed(KEY_S):
 		mypos.y += 350 * delta
 		set_z(mypos.y)
 	
